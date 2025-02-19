3629484d88d54badd78ed79f9c23df585140afd6
Marcio Taier <marcinhotulio@hotmail.com>
2025-02-19 00:32:09 -0300

    Bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { CarrinhoProvider } from "./context/carrinhoContext";
+import { CarrinhoProvider } from "./context/CarrinhoContext";
 import Home from "./pages/Home";
 import Carrinho from "./pages/Carrinho";
 import PaginaErro from "./pages/PaginaErro";
